b198de7ba4bad16649e6d40115ecc5db6cb2cd9b
Huynh Thi Khanh Linh <bongudth@users.noreply.github.com>
2022-06-10 11:28:19 +0700

    :zap: build models for books

--- books/models.py
+++ books/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    category = models.ForeignKey(Category, related_name='book_category', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    image_url = models.URLField(blank=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    is_available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'books'
+        ordering = ('-created_at',)
+
+    def __str__(self):
+        return self.title
