c3f14bedc432a2de1636b4567fdb2093d2a2bfcc
Huynh Thi Khanh Linh <bongudth@users.noreply.github.com>
2022-06-14 18:33:21 +0700

    :art: build the book detail page template

--- books/urls.py
+++ books/urls.py
@@ -5,4 +5,5 @@ app_name = 'books'
 
 urlpatterns = [
     path('', views.all_books, name='all_books'),
+    path('book/<slug:slug>/', views.book_detail, name='book_detail'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Category, Book
 
 # Create your views here.
@@ -10,4 +10,8 @@ def categories(request):
 
 def all_books(request):
     books = Book.objects.all()
-    return render(request, 'books/home.html', {'books': books})
+    return render(request, 'library/home.html', {'books': books})
+
+def book_detail(request, slug):
+    book = get_object_or_404(Book, slug=slug)
+    return render(request, 'library/books/detail.html', {'book': book})
