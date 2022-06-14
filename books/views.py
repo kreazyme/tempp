3cfb9854e8361f1afb6be280f908b9f107bb09ca
Huynh Thi Khanh Linh <bongudth@users.noreply.github.com>
2022-06-14 20:25:20 +0700

    :bug: fix bug in context processors

--- books/views.py
+++ books/views.py
@@ -3,6 +3,11 @@ from .models import Category, Book
 
 # Create your views here.
 
+def categories(request):
+    return {
+        'categories': Category.objects.all()
+    }
+
 def all_books(request):
     books = Book.objects.all()
     return render(request, 'library/home.html', {'books': books})
