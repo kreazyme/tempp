c12831c5009d39bce9f31207184299df582f8b9e
Huynh Thi Khanh Linh <bongudth@users.noreply.github.com>
2022-06-14 21:49:28 +0700

    :wrench: config context processors

--- library/settings.py
+++ library/settings.py
@@ -64,7 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'books.views.categories',
+                'books.context_processors.categories',
             ],
         },
     },
