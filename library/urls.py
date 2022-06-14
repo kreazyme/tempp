19f1b9799275934657cd9c2bb69fae7c0e8c8884
Huynh Thi Khanh Linh <bongudth@users.noreply.github.com>
2022-06-14 20:17:57 +0700

    :lipstick: add favicon.ico

--- library/urls.py
+++ library/urls.py
@@ -17,10 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('books.urls', namespace='books')),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('images/favicon.ico'))),
 ]
 
 if settings.DEBUG:
